cd462c133ef8d7ecef2f749e96e542488cc7bb88
Laye Mehta <layeamehta@gmail.com>
2022-09-08 23:30:14 -0600

    added config for publisher and created service activator to publish to outbound adapter

---
+++ publisher/src/main/kotlin/com/pubsub/publisher/pubsub/PubSubConfiguration.kt
@@ -0,0 +1,22 @@
+package com.pubsub.publisher.pubsub
+
+import com.google.cloud.spring.pubsub.core.PubSubTemplate
+import com.google.cloud.spring.pubsub.integration.outbound.PubSubMessageHandler
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.annotation.ServiceActivator
+import org.springframework.messaging.MessageHandler
+
+const val DEMO_CHANNEL = "demoChannel"
+
+@Configuration
+class PubSubConfiguration(
+    @Value("\${openhouse.pubsub.topics.demoTopic}") private val demoTopic: String
+) {
+    @Bean
+    @ServiceActivator(inputChannel = DEMO_CHANNEL)
+    fun demoMessageSender(pubSubTemplate: PubSubTemplate): MessageHandler {
+        return PubSubMessageHandler(pubSubTemplate, demoTopic)
+    }
+}
